321f326c26e060f43281ed30ff6c2f6469ef291b
Charlene (Chay) Makwara <158230758+MakChay@users.noreply.github.com>
2026-06-20 23:00:09 +0000

    Added icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,19 +9,25 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'House of Glory - Data Collection System',
   description: 'Professional data collection and management system for House of Glory.',
+  
+  icons: {
+  icon: '/afm_big_logo.png',
+},
+  
   openGraph: {
     images: [
-     {
-        url: "https://afm-house-of-glory-member-form.vercel.app//afm_big_logo.png",
+      {
+        url: 'https://afm-house-of-glory-member-form.vercel.app/afm_big_logo.png',
         width: 1200,
         height: 630,
-        alt: "AFM House of Glory",
+        alt: 'AFM House of Glory',
       },
     ],
   },
   twitter: {
     card: 'summary_large_image',
-    images: [ "https://afm-house-of-glory-member-form.vercel.app//afm_big_logo.png"
+    images: [
+      'https://afm-house-of-glory-member-form.vercel.app/afm_big_logo.png',
     ],
   },
 };
